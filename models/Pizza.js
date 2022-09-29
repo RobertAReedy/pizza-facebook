1d781ec8dfa36d719809a0e193874e16bb6ad8ca
RobertAReedy <robertandreedy@gmail.com>
2022-09-29 15:00:19 -0400

    Comment model created, relation to Pizza added to Pizza model

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,24 +1,47 @@
 const { Schema, model } = require("mongoose");
 
-const PizzaSchema = new Schema({
-  pizzaName: {
-    type: String
+const PizzaSchema = new Schema(
+  {
+    pizzaName: {
+      type: String
+    },
+    createdBy: {
+      type: String
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    },
+    size: {
+      type: String,
+      default: "Large"
+    },
+    toppings: [],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Comment"
+      }
+    ]
   },
-  createdBy: {
-    type: String
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  },
-  size: {
-    type: String,
-    default: "Large"
-  },
-  toppings: []
-  //,toppings: { type: Array } //would this work?
-});
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false
+  }
+);
 
+/**
+ * Rather than storing a value in the database, which
+ * would need to be updated every time a comment was
+ * added, we can simply add this virtual, which will
+ * activate and calculate the 'comment count' by
+ * using the length of the comments array
+ */
+PizzaSchema.virtual("commentCount").get(function () {
+  return this.comments.length;
+});
 const Pizza = model("Pizza", PizzaSchema);
 
 module.exports = Pizza;
