aa491712162a6e0da219abb60dddd0d71badb607
RobertAReedy <robertandreedy@gmail.com>
2022-10-05 12:54:05 -0400

    uh, actually, comments just DISPLAY on the front end. Is what I meant.

--- models/Comment.js
+++ models/Comment.js
@@ -13,7 +13,7 @@ const CommentSchema = new Schema({
   createdAt: {
     type: Date,
     default: Date.now,
-    get: createdAtVal => dateFormat(createdAtVal)
+    get: createdAtVal => dateFormat(createdAtVal) 
   }
 },
 {
